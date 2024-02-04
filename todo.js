1f985427ad32bbb18d9468b89accc4670dce49fb
Vikrant <vikrant4514@gmail.com>
2024-02-04 18:52:51 +0530

    o

--- todo.js
+++ todo.js
@@ -1,5 +1,4 @@
 let todoList = [
-    
   ];
   
   displayItems();
@@ -10,24 +9,45 @@ let todoList = [
     let todoItem = inputElement.value;
     let todoDate = dateElement.value;
 
+
     if(todoDate === "" || todoItem === "") return;
     const d = todoDate.split('-')
     todoDate = `${d[2]}/${d[1]}/${d[0]}`
     todoList.push({item: todoItem, dueDate: todoDate});
+
+    const localtodo = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : []
+    localtodo.push({
+      todo: todoItem,
+      date: todoDate
+    })
+    localStorage.setItem("todos",JSON.stringify(localtodo))
     inputElement.value = '';
     dateElement.value = '';
     displayItems();
   }
   
+  function deleteTodo(i){
+    
+    todoList = todoList.filter((_, index) => index !== i);
+   
+    localStorage.setItem("todos",JSON.stringify(todoList))
+    displayItems()
+  }
   function displayItems() {
+    const localtodo = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : []
+    if(localtodo?.length > 0){
+      
+      todoList = []
+      todoList.push(...localtodo)
+    }
     let containerElement = document.querySelector('.todo-container');
     let newHtml = '';
     for (let i = 0; i < todoList.length; i++) {
-      let {item, dueDate} = todoList[i];
+      let {todo, date} = todoList[i];
       newHtml += `
-        <span>${item}</span>
-        <span>${dueDate}</span>
-        <button class='btn-delete' onclick="todoList.splice(${i}, 1);
+        <span>${todo}</span>
+        <span>${date}</span>
+        <button class='btn-delete' onclick="deleteTodo(${i})";
         displayItems();">Delete</button>
       `;
     }
